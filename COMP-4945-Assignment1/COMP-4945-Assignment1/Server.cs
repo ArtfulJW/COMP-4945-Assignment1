6b10ee7fa711b207bd33d59ad58b54cbafb88e95
Tissure <kyle.ng7512@gmail.com>
2023-01-06 13:00:58 -0800

    Create client project and commented DirClient and DirServer Code for learning use

--- COMP-4945-Assignment1/COMP-4945-Assignment1/Server.cs
+++ COMP-4945-Assignment1/COMP-4945-Assignment1/Server.cs
@@ -22,7 +22,7 @@
  */
 
 /*
- * ================== DirServer.cs ==================
+//* ================== DirServer.cs ==================
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -33,43 +33,68 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
-public class DirListing
-{
+public class DirListing {
+    //This is the client socket
     Socket cls = null;
-    public DirListing(Socket socket) { this.cls = socket; }
-    public void threadMethod()
-    {
+    public DirListing(Socket socket) {
+        this.cls = socket;
+    }
+    public void threadMethod() {
+        //Set character buffer of one byte
         Byte[] bytesReceived = new Byte[1];
         string a = "";
-        while (true)
-        {
-            if ((cls.Receive(bytesReceived, bytesReceived.Length, 0) == 0) ||
-             (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0'))
-            {
+
+        //
+        while (true) {
+            //Read one byte from socket and get number of bytes read
+            int recv = cls.Receive(bytesReceived, bytesReceived.Length, 0);
+            //If recv is 0 the connection is closed
+            bool isClosed = (recv == 0);
+            //Check if client connection closed or end line received
+            if ( isClosed || (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0')) {
+                // Exit while loop
                 break;
             }
             // string a should now have the whole http request saved.s
             a += Encoding.ASCII.GetString(bytesReceived, 0, 1);
         }
+        
         Console.WriteLine(a);
+        
         DirectoryInfo di = new DirectoryInfo(a);
+        //Get list of files in di as array
         FileInfo[] fiArr = di.GetFiles();
         string files = "";
-        foreach (FileInfo fri in fiArr) { files = files + fri.Name; }
+        //Get every file name in fiArr
+        foreach (FileInfo fri in fiArr) {
+            files = files + fri.Name;
+        }
+
+        //Convert files into HTTP byte format
         byte[] msg = System.Text.Encoding.ASCII.GetBytes(files + '\0');
+        //send msg to client
         cls.Send(msg, msg.Length, 0);
+        //close client socket
         cls.Close();
     }
-    static void Main(string[] args)
-    {
-        try
-        {
-            int port = 8888; IPAddress address = IPAddress.Parse("127.0.0.1");
+    static void Main(string[] args) {
+        try {
+            int port = 8888;
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+
+            // Create EndPoint object from port and ipAddress
             IPEndPoint ipe = new IPEndPoint(address, port);
+
+            // Create serverside listener socket
             Socket s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            s.Bind(ipe); s.Listen(10);
-            while (true)
-            {
+
+            // Bind socket to EndPoint
+            s.Bind(ipe);
+
+            // Set socket to listen for upto 10 connection
+            s.Listen(10);
+
+            while (true) {
                 // Accept is a blocking call
                 Socket cls = s.Accept();
                 DirListing dirListing = new DirListing(cls);
@@ -77,16 +102,15 @@ public class DirListing
                 Thread thread = new Thread(new ThreadStart(dirListing.threadMethod));
                 thread.Start();
             }
+            // Close server socket
             s.Close();
-        }
-        catch (SocketException e)
-        {
+        } catch (SocketException e) {
             Console.WriteLine("Socket exception: {0}", e);
         }
     }
-}
-================== DirServer.cs ==================
-*/
+}*/
+//================== DirServer.cs ==================
+
 
 
 using System;
@@ -96,19 +120,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 // Namespace is a container for related classes
-namespace Program 
-{
-    class Program 
-    { 
+namespace Program {
+    class Program {
 
-        public static void ExecuteServer()
-        {
+        public static void ExecuteServer() {
 
         }
 
-        static void Main(string[] args)
-        {
-            
+        static void Main(string[] args) {
+
         }
     }
 }
---
+++ COMP-4945-Assignment1/Client/Client.cs
@@ -0,0 +1,83 @@
+﻿
+/*//============= DirClient.cs =============
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DirClient {
+    private IPEndPoint ipe;
+
+    public DirClient(string ipaddr, int port) {
+        this.ipe = new IPEndPoint(IPAddress.Parse(ipaddr), port);
+    }
+
+    public string getListing(string path) {
+        string a = "";
+        try {
+            //Create client socket
+            Socket s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine(path);
+
+            // Connect client socket to EndPoint
+            s.Connect(ipe);
+
+            if (s.Connected) {
+                // Encode path to byte array
+                Byte[] bytesSent = Encoding.ASCII.GetBytes(path + '\0');
+                // Send over client socket to EndPoint bytesSent 
+                s.Send(bytesSent, bytesSent.Length, 0);
+
+                //Set character buffer of one byte
+                Byte[] bytesReceived = new Byte[1];
+
+                while (true) {
+                    //Read one byte from socket and get number of bytes read
+                    int recv = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                    //If recv is 0 the connection is closed
+                    bool isClosed = (recv == 0);
+                    //Check if server connection closed or end line received
+                    if (isClosed || (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0')) {
+                        // Exit while loop
+                        break;
+                    }
+                    a += Encoding.ASCII.GetString(bytesReceived, 0, 1);
+                }
+            }
+            // Close client socket
+            s.Close();
+            return a;
+        } catch (ArgumentNullException e) {
+            throw new Exception(@"[ArguementNullException From Sync.DirClient]",
+                e);
+        } catch (SocketException e) {
+            throw new Exception(@"[SocketException From Sync.DirClient]", e);
+        }
+    }
+
+    static void Main(string[] args) {
+        // Create DirClient obj with intended EndPoint ipAddress and port number
+        DirClient myDirClient = new DirClient("127.0.0.1", 8888);
+        Console.WriteLine(myDirClient.getListing("c:\\Windows"));
+
+        //the following call is just to block the main thread so that the results are listed to the screen
+        Console.Read();
+    }
+}
+==========================*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    internal class Client {
+        static void Main(string[] args) {
+        }
+    }
+}
