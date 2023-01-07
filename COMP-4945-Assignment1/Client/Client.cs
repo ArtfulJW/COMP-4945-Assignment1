dc8cccc0ef0242245bfe2c57eddf2f3fc839f508
Kyle <kyle.ng7512@gmail.com>
2023-01-06 19:18:34 -0800

    Implement user input

--- COMP-4945-Assignment1/Client/Client.cs
+++ COMP-4945-Assignment1/Client/Client.cs
@@ -81,45 +81,72 @@ using System.Threading.Tasks;
 namespace Client {
 	internal class Client {
 		private IPEndPoint ipe;
+		private Socket s;
 		public Client(string ipaddr, int port) {
 			this.ipe = new IPEndPoint(IPAddress.Parse(ipaddr), port);
 		}
 
-		void uploadImage() {
+		private void dumpSocket() {
+			//Set character buffer of one byte
+			Byte[] bytesReceived = new Byte[1];
+			string a = "";
+			while (true) {
+				//Read one byte from socket and get number of bytes read
+				int recv = s.Receive(bytesReceived, bytesReceived.Length, 0);
+				//If recv is 0 the connection is closed
+				bool isClosed = (recv == 0);
+				//Check if server connection closed or end line received
+				if (isClosed || (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0')) {
+					// Exit while loop
+					break;
+				}
+				a = a + Encoding.ASCII.GetString(bytesReceived, 0, 1);
+			}
+			//Parse response
+			Console.WriteLine(a);
+		}
+
+		private void getUserInput(ref string filePath, ref string caption, ref string date) {
+			do {
+				Console.WriteLine("Enter file path for image: ");
+				filePath = Console.ReadLine();
+			} while (Validator.isValidFilePath(filePath));
+			do {
+				Console.WriteLine("Enter caption for image: ");
+				caption = Console.ReadLine();
+			} while (Validator.isValidCaption(caption));
+			do {
+				Console.WriteLine("Enter date for image: ");
+				date = Console.ReadLine();
+			} while (Validator.isValidDate(date));
+			
+		}
+
+		private void uploadImage() {
 			try {
-				Socket s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 				s.Connect(ipe);
 				if (s.Connected) {
 					Console.WriteLine("Connected");
 					//do file upload process
 					//Get user input
+					string filePath = String.Empty;
+					string caption = String.Empty;
+					string date = String.Empty;
+					getUserInput(ref filePath, ref caption, ref date);
 					//Validate user input
 					//Build request
+					HttpRequestBuilder.buildMultipartRequest(filePath, caption, date);
 					//Send request
 					//Get response
-
-					//Set character buffer of one byte
-					Byte[] bytesReceived = new Byte[1];
-					string a = "";
-					while (true) {
-						//Read one byte from socket and get number of bytes read
-						int recv = s.Receive(bytesReceived, bytesReceived.Length, 0);
-						//If recv is 0 the connection is closed
-						bool isClosed = (recv == 0);
-						//Check if server connection closed or end line received
-						if (isClosed || (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0')) {
-							// Exit while loop
-							break;
-						}
-						a += Encoding.ASCII.GetString(bytesReceived, 0, 1);
-					}
-					//Parse response
-					Console.WriteLine(a);
+					dumpSocket();
 				}
 				s.Close();
-			} catch (ArgumentNullException e) {
+			}
+			catch (ArgumentNullException e) {
 				throw new Exception(@"[ArguementNullException From Sync.DirClient]", e);
-			} catch (SocketException e) {
+			}
+			catch (SocketException e) {
 				throw new Exception(@"[SocketException From Sync.DirClient]", e);
 			}
 		}
