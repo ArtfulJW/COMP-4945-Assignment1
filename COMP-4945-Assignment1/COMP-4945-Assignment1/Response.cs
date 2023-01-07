2f83af60eaa558f45f2ad871739b78ca496a1204
Jay Wang <jwang550@my.bcit.ca>
2023-01-07 15:00:54 -0800

    Setting things up for next steps

---
+++ COMP-4945-Assignment1/COMP-4945-Assignment1/Response.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Response
+    {
+        // Member Variables
+        string indexHTML = "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 600\n\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title> File Upload Form</title>\r\n</head>\r\n<body>\r\n<h1>Upload file</h1>\r\n<form id =\"form\" method=\"POST\" action=\"/\" enctype=\"multipart/form-data\">\r\n<input type=\"file\" name=\"fileName\"/><br/><br/>\r\nCaption: <input type =\"text\" name=\"caption\"<br/><br/>\r\n <br/>\nDate : <input type=\"date\" name=\"date\"<br/><br/>\r\n <br/>\n <input id='formBtn' type=\"submit\" name=\"submit\" value=\"Submit\"/>\r\n </form>\r\n</body>\r\n</html>\r\n";
+        Socket socket = null;
+        Servlet servlet = null;
+
+        // Constructor
+        public Response(Socket socket, Servlet servlet)
+        {
+            this.socket = socket;
+            this.servlet = servlet;
+        }
+
+        public void renderHTML()
+        {
+            byte[] message = System.Text.Encoding.ASCII.GetBytes(indexHTML + '\0');
+            socket.Send(message, message.Length, 0);
+        }
+
+    }
+}
