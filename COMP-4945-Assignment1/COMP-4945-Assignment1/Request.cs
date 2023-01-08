b0d5da7af249e73e6d63c9212d4e3656411e0ab3
james <jan31@my.bcit.ca>
2023-01-08 04:21:00 -0800

    parse header and body store each line as key value in Dictionary

---
+++ COMP-4945-Assignment1/COMP-4945-Assignment1/Request.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_4945_Assignment1
+{
+    internal class Request
+    {
+        Socket cls = null;
+        public Request(Socket socket) { this.cls = socket; }
+        public void threadMethod()
+        {
+            Byte[] bytesReceived = new Byte[1];
+            string a = "";
+            while (true)
+            {
+                if ((cls.Receive(bytesReceived, bytesReceived.Length, 0) == 0) ||
+                 (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\r'))
+                {
+                    break;
+                }
+                a += Encoding.ASCII.GetString(bytesReceived, 0, 1);
+            }
+            Console.WriteLine(a + " this ");
+
+            string[] req = a.Split('\n');
+            string[] header = req[0].Split(' ');
+            req = req.Skip(1).ToArray();
+            string reqType = header[0];
+            string URL = header[1];
+            string version = header[2];
+
+            //Console.WriteLine(req[0]);
+            //Console.WriteLine(URL);
+            //Console.WriteLine(version);
+
+            //Console.WriteLine(lines[0]);
+
+            Dictionary<string, string> map = new Dictionary<string, string>();
+
+            /*  for(int i = 0; i < lines.Length-2 ; i++)
+              {
+                  string key = lines[i].Split(':')[0].Trim();
+                  string val = lines[i].Split(':')[1].Trim();
+                  Console.WriteLine("Key = " + key);
+                  Console.WriteLine("Val = " + val);
+                  map.Add(key, val);
+              }*/
+            foreach (string line in req)
+            {
+                if (line != null)
+                {
+                    Console.WriteLine(line + "a");
+                    /*      string key = line.Split(':')[0].Trim();
+                          string val = line.Split(':')[1].Trim();
+                          Console.WriteLine(key);
+                          Console.WriteLine(val);
+                          map.Add(key, val);*/
+                }
+            }
+            //Console.WriteLine("user agent = " +  map["User-Agent"]);
+
+            /*      DirectoryInfo di = new DirectoryInfo(a);
+                  FileInfo[] fiArr = di.GetFiles();
+                  string files = "";
+                  foreach (FileInfo fri in fiArr) { files = files + fri.Name; }*/
+
+
+            string files = """
+            HTTP/1.1 200 ok
+            Content-Type: text/html
+            
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>File Upload Form</title>
+                </head>
+                <body>
+            <h1>Upload file</h1>
+            <form method="POST" action="/" enctype="multipart/form-data">
+            <input type="file" name="fileName"/><br/><br/>
+            Caption: <input type="text" name="caption"<br/><br/>
+            <br />
+            Date: <input type="date" name="date"<br/><br/>
+            <br />
+            <input type="submit" value="Submit"/>
+            </form>
+            </body>
+            </html>
+            """;
+            //foreach (FileInfo fri in fiArr) { files = files + fri.Name; }
+            byte[] msg = Encoding.ASCII.GetBytes(files);
+
+            cls.Send(msg, msg.Length, 0);
+            cls.Close();
+        }
+    }
+}
